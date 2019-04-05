5a3532f022ee9f8e79d96de0ec068a6265e1c5c1
shuchao <645904877@qq.com>
2019-04-06 01:12:19 +0800

    fix bug

--- logger.go
+++ logger.go
@@ -19,7 +19,7 @@ func getLoggerMust() *zap.Logger {
 	return logger
 }
 
-func WithOptions(opts ...zap.Options) *zap.Logger { return getLoggerMust().WithOptions(opts) }
+func WithOptions(opts ...zap.Option) *zap.Logger { return getLoggerMust().WithOptions(opts) }
 
 func Check(lvl zapcore.Level, msg string) *zapcore.CheckedEntry {
 	return getLoggerMust().Check(lvl, msg)
@@ -27,27 +27,27 @@ func Check(lvl zapcore.Level, msg string) *zapcore.CheckedEntry {
 
 func Core() zapcore.Core { return getLoggerMust().Core() }
 
-func DPanic(msg string, fields ...zap.Field) { getLoggerMust().DPanic(msg, fields) }
+func DPanic(msg string, fields ...zap.Field) { getLoggerMust().DPanic(msg, fields...) }
 
-func Debug(msg string, fields ...zap.Field) { getLoggerMust().Debug(msg, fields) }
+func Debug(msg string, fields ...zap.Field) { getLoggerMust().Debug(msg, fields...) }
 
-func Error(msg string, fields ...zap.Field) { getLoggerMust().Error(msg, fields) }
+func Error(msg string, fields ...zap.Field) { getLoggerMust().Error(msg, fields...) }
 
-func Fatal(msg string, fields ...zap.Field) { getLoggerMust().Fatal(msg, fields) }
+func Fatal(msg string, fields ...zap.Field) { getLoggerMust().Fatal(msg, fields...) }
 
-func Info(msg string, fields ...zap.Field) { getLoggerMust().Info(msg, fields) }
+func Info(msg string, fields ...zap.Field) { getLoggerMust().Info(msg, fields...) }
 
 func Named(s string) *zap.Logger { return getLoggerMust().Named(s) }
 
-func Panic(msg string, fields ...zap.Field) { getLoggerMust().Panic(msg, fields) }
+func Panic(msg string, fields ...zap.Field) { getLoggerMust().Panic(msg, fields...) }
 
 func Sugar() *zap.SugaredLogger { return getLoggerMust().Sugar() }
 
 func Sync() error { return getLoggerMust().Sync() }
 
-func Warn(msg string, fields ...zap.Field) { getLoggerMust().Warn(msg, fields) }
+func Warn(msg string, fields ...zap.Field) { getLoggerMust().Warn(msg, fields...) }
 
-func With(fields ...zap.Field) *zap.Logger { return getLoggerMust().With(fields) }
+func With(fields ...zap.Field) *zap.Logger { return getLoggerMust().With(fields...) }
 
 func Register(l *zap.Logger) *zap.Logger {
 	mutex.Lock()
