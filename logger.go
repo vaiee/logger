aeed0c272eef210a933a6820ce5fe54f3f9bba94
shuchao <645904877@qq.com>
2019-04-06 01:09:52 +0800

    global logger

---
+++ logger.go
@@ -0,0 +1,62 @@
+package logger
+
+import (
+	"sync"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+var (
+	mutex  sync.RWMutex
+	logger *zap.Logger
+)
+
+func getLoggerMust() *zap.Logger {
+	if logger == nil {
+		panic("logger: logger is nil (forgotten register?)")
+	}
+	return logger
+}
+
+func WithOptions(opts ...zap.Options) *zap.Logger { return getLoggerMust().WithOptions(opts) }
+
+func Check(lvl zapcore.Level, msg string) *zapcore.CheckedEntry {
+	return getLoggerMust().Check(lvl, msg)
+}
+
+func Core() zapcore.Core { return getLoggerMust().Core() }
+
+func DPanic(msg string, fields ...zap.Field) { getLoggerMust().DPanic(msg, fields) }
+
+func Debug(msg string, fields ...zap.Field) { getLoggerMust().Debug(msg, fields) }
+
+func Error(msg string, fields ...zap.Field) { getLoggerMust().Error(msg, fields) }
+
+func Fatal(msg string, fields ...zap.Field) { getLoggerMust().Fatal(msg, fields) }
+
+func Info(msg string, fields ...zap.Field) { getLoggerMust().Info(msg, fields) }
+
+func Named(s string) *zap.Logger { return getLoggerMust().Named(s) }
+
+func Panic(msg string, fields ...zap.Field) { getLoggerMust().Panic(msg, fields) }
+
+func Sugar() *zap.SugaredLogger { return getLoggerMust().Sugar() }
+
+func Sync() error { return getLoggerMust().Sync() }
+
+func Warn(msg string, fields ...zap.Field) { getLoggerMust().Warn(msg, fields) }
+
+func With(fields ...zap.Field) *zap.Logger { return getLoggerMust().With(fields) }
+
+func Register(l *zap.Logger) *zap.Logger {
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	if logger == nil {
+		logger = l
+		return logger
+	}
+
+	panic("logger: Register called twice for logger")
+}
